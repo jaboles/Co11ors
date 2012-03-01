06d4e476e4f9ae417b7f24088495c420d7a58838
jb <jb@cc9c2eb6-ac48-d949-9988-e994760dfea6>
2012-03-01 05:13:00 +0000

    Some utility stuff
    git-svn-id: https://jbpc/svn/Co11ors@15 cc9c2eb6-ac48-d949-9988-e994760dfea6

---
+++ VSIconSwitcher/ExtensionMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using Mono.Cecil;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+namespace VSIconSwitcher
+{
+    public static class ExtensionMethods
+    {
+        public static bool ContainsResource(this ResourceReader @this, string resName)
+        {
+            try
+            {
+                string dummy1;
+                byte[] dummy2;
+                @this.GetResourceData(resName, out dummy1, out dummy2);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        public static UnmanagedMemoryStream FindResource(this ResourceReader @this, string resName)
+        {
+            IDictionaryEnumerator e = @this.GetEnumerator();
+            while (e.MoveNext())
+            {
+                string key = e.Key as string;
+                Debug.Assert(key != null);
+
+                if (key.Equals(resName))
+                {
+                    UnmanagedMemoryStream data = e.Value as UnmanagedMemoryStream;
+                    Debug.Assert(data != null);
+                    return data;
+                }
+            }
+            Debug.Fail("Should never get here. Couldn't find a matching resource for name " + resName);
+            return null;
+        }
+
+        public static void ReplaceResource(this AssemblyDefinition @this, string resName, byte[] newData)
+        {
+            Resource resourceToReplace = @this.MainModule.Resources.Single(r => r.Name.Equals(resName, StringComparison.OrdinalIgnoreCase));
+            ManifestResourceAttributes attribs = resourceToReplace.Attributes;
+            int index = @this.MainModule.Resources.IndexOf(resourceToReplace);
+
+            @this.MainModule.Resources.RemoveAt(index);
+            @this.MainModule.Resources.Insert(index, new EmbeddedResource(resName, attribs, newData));
+        }
+    }
+}
---
+++ VSIconSwitcher/LZWBitmap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VSIconSwitcher
+{
+    public class LZWBitmap
+    {
+        private int m_width;
+        private int m_height;
+        private int m_type;
+
+        public LZWBitmap()
+        {
+        }
+
+        public void Load(MemoryStream ms)
+        {
+            m_width = ms.ReadByte() + (ms.ReadByte() << 8);
+            m_height = ms.ReadByte() + (ms.ReadByte() << 8);
+            m_height = ms.ReadByte() + (ms.ReadByte() << 8);
+        }
+    }
+}
